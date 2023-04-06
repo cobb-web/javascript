7025ac16c6a257b658bc9af87fd0e03e136d8d33
Prince Cobb <122997450+cobb-web@users.noreply.github.com>
2023-04-06 16:45:07 -0400

    WK13 Exercise Completed

--- week13exercise/obj.js
+++ week13exercise/obj.js
@@ -2,10 +2,11 @@ var Car = {
     brand: "Mazda",
     model: "CX",
     suspension: "good",
-    smartCar: true
+    smartCar: true,
+    display: "4k"
 }
 
-var CarJSON = '{"brand":"Mazda","model":"CX","suspension":"good","smartCar":true}';
+var CarJSON = '{"brand":"Mazda","model":"CX","suspension":"good","smartCar":true, "display":"4k"}';
 
 // var CarStringified = JSON.stringify(Car);
 
